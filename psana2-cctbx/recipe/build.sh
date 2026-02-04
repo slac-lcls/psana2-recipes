0b504f72ef812ebd3f85aa989e9a352b4f0fda71
Valerio Mariani <valmar@user.noreply.github.com>
2026-02-04 14:30:03 -0800

    Added special psana for cctbx

---
+++ psana2-cctbx/recipe/build.sh
@@ -0,0 +1,11 @@
+# Capture the install flag
+make_install=$1
+
+# Create and navigate to the build directory
+mkdir -p build
+cd build
+
+# Run CMake configuration with the remaining arguments
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${PREFIX}" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DNO_DAQ=TRUE ..
+make -j 4
+make -j 4 install
